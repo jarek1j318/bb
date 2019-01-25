4b4fc1a3f79367e4d7b3a4682e5853133e437a5a
Jarosław Herman <jaroslaw.herman@asseco.pl>
2019-01-25 15:49:55 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log('test works');
+console.log('OK-EKS');
+
+function loop() {
+    console.log('loop works');
+    let c = confirm('przywrócić domyślne podstawy ?');
+    console.log(`c = ${c}`);
+    // set('20px', '20px');
+    // set('20px', '800px');
+    // set('600px', '20px');
+    set('600px', '800px');
+}
+
+function set(top, left) {
+    i = document.getElementById("img");
+    i.style.top=top;
+    i.style.left=left;
+}
