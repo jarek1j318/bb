e412021accb9472c87c9a5f442634b30834397e4
jarek1j318 <mjh1@tlen.pl>
2019-09-27 13:03:52 +0200

    newest one

--- test.js
+++ test.js
@@ -1,28 +1,68 @@
-console.log('test works');
-console.log('OK-EKS');
+document.onkeydown = loop;
 
-async function loop() {
-    let test = getTable(8);
-    for(i = 0; i < test.length; i++){
-        show(test[i]);
-        await sleep(200);
-        hide();
-        await sleep(600);
+async function loop(e) {
+    if(e.code != 'ArrowDown' && e.code != 'Space' && e.code != 'ArrowUp') return;
+    clear();
+    await sleep(7000);
+    let shots = getTable(e);
+    for(i = 0; i < shots.length; i++){
+        showReady();
+        await sleep(170);
+        show(shots[i]);
+        await sleep(800);
+        hide('ready');
+        hide('img');
+        await sleep(getInterval(e));
     }
     success();
 }
 
-function getTable(n) {
+function clear() {
+    hide('ready');
+    hide('img');
+    hide('success');
+}
 
+function getInterval(e){
+    let factor = 1.1;
+    let interval = 2500;
+    if(e.code === 'ArrowDown'){
+        interval = 2700;
+    }
+    if(e.code === 'ArrowUp'){
+        interval = 2500;
+    }
+    return interval * factor;
+}
+
+function getTable(e) {
+
+    let x0 = 1;
+    let xk = 4;
+    console.log(e);
+    if(e.code === 'ArrowDown'){
+        x0 = 3;
+    }
+    if(e.code === 'ArrowUp'){
+        xk = 2;
+    }
+    let n = 12;
     let t = [];
     for( i = 0; i < n; i++){
-        let a = getRandomInt(1, 5);
+        let a = getRandomInt(x0, xk+1);
         t.push(a);
     }
 
     console.log(t);
     // let t = [1, 3, 2, 4, 4, 1, 3];
     return t;
+}
+
+function showReady() {
+    const img = document.getElementById("ready");
+    img.style.visibility = 'visible';
+    img.style.top = '100px';
+    img.style.left = '400px';
 }
 
 function show(i) {
@@ -35,12 +75,12 @@ function show(i) {
         left='20px';
     } else if (i === 2) {
         top='20px';
-        left='800px';
+        left='1100px';
     } else if (i === 3) {
-        top='600px';
-        left='800px';
+        top='500px';
+        left='1100px';
     } else if (i === 4) {
-        top='600px';
+        top='500px';
         left='20px';
     }
     const img = document.getElementById("img");
@@ -49,8 +89,8 @@ function show(i) {
     img.style.left = left;
 }
 
-function hide() {
-    const img = document.getElementById("img");
+function hide(id) {
+    const img = document.getElementById(id);
     img.style.visibility = 'hidden';
 }
 
@@ -67,4 +107,6 @@ function getRandomInt(min, max) {
   function success() {
     const img = document.getElementById("success");
     img.style.visibility = 'visible';
+    img.style.top = '20px';
+    img.style.left = '300px';
   }
